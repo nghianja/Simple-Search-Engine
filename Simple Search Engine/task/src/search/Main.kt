7127ff5041b39a075caa09836d2a0d2ed418bc7c
James Ng <james.ng@projapps.com>
2020-03-11 16:51:19 +0800

    Complete Stage #2

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,12 +1,28 @@
 package search
 
 fun main() {
-    val lines = readLine()!!.split(" ")
-    val line = readLine()!!
-    for (i in 1..lines.size)
-        if (line == lines[i - 1]) {
-            println(i)
-            return
+    println("Enter the number of people:")
+    var number = readLine()!!.toInt()
+    val data = arrayOfNulls<String>(number)
+    println("Enter all people:")
+    for (i in 1..number) {
+        data[i - 1] = readLine()!!
+    }
+    println("Enter data to search people:")
+    number = readLine()!!.toInt()
+    for (i in 1..number) {
+        var found = false
+        val search = readLine()!!
+        for (j in data.indices) {
+            if (data[j]!!.contains(search, true)) {
+                if (!found) {
+                    found = true
+                    println("Found people:")
+                }
+                println(data[j])
+            }
         }
-    println("Not found")
+        if (!found)
+        println("No matching people found.")
+    }
 }
