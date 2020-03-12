39212325c309f8e3f1e36a83070b983b0a8b5443
James Ng <james.ng@projapps.com>
2020-03-12 17:39:50 +0800

    Complete Stage #3

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -1,28 +1,46 @@
 package search
 
 fun main() {
+    val data = enterData()
+    println()
+    loop@ while (true) {
+        println("=== Menu ===")
+        println("1. Find a person")
+        println("2. Print all people")
+        println("0. Exit")
+        println()
+        when (readLine()!!.toInt()) {
+            1 -> findPerson(data)
+            2 -> printAll(data)
+            0 -> break@loop
+            else -> println("Incorrect option! Try again.")
+        }
+    }
+    println("Bye!")
+}
+
+private fun printAll(data: Array<String?>) {
+    println("=== List of people ===")
+    data.forEach { println(it) }
+}
+
+private fun findPerson(data: Array<String?>) {
+    println("Enter a name or email to search all suitable people.")
+    val search = readLine()!!
+    data.forEach {
+        if (it!!.contains(search, true)) {
+            println(it)
+        }
+    }
+}
+
+private fun enterData(): Array<String?> {
     println("Enter the number of people:")
-    var number = readLine()!!.toInt()
+    val number = readLine()!!.toInt()
     val data = arrayOfNulls<String>(number)
     println("Enter all people:")
     for (i in 1..number) {
         data[i - 1] = readLine()!!
     }
-    println("Enter data to search people:")
-    number = readLine()!!.toInt()
-    for (i in 1..number) {
-        var found = false
-        val search = readLine()!!
-        for (j in data.indices) {
-            if (data[j]!!.contains(search, true)) {
-                if (!found) {
-                    found = true
-                    println("Found people:")
-                }
-                println(data[j])
-            }
-        }
-        if (!found)
-        println("No matching people found.")
-    }
+    return data
 }
