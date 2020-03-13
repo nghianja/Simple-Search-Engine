15cc7dc18ca9784e952b7bb572380b00046d33f1
James Ng <james.ng@projapps.com>
2020-03-13 12:43:20 +0800

    Complete Stage #6

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -7,38 +7,69 @@ fun main(args: Array<String>) {
         if (args.size == 2 && args[0] == "--data") importData(args[1])
         else enterData()
     val index = createInvertedIndex(data)
-    println()
     loop@ while (true) {
         println("=== Menu ===")
         println("1. Find a person")
         println("2. Print all people")
         println("0. Exit")
-        println()
         when (readLine()!!.toInt()) {
             1 -> findPerson(data, index)
             2 -> printAll(data)
             0 -> break@loop
             else -> println("Incorrect option! Try again.")
         }
+        println()
     }
     println("Bye!")
 }
 
 private fun printAll(data: List<String>) {
+    println()
     println("=== List of people ===")
     data.forEach { println(it) }
 }
 
-private fun findPerson(data: List<String>, index: Map<String, MutableList<Int>>) {
-    println("Enter a name or email to search all suitable people.")
-    val search = readLine()!!
-    if (index[search] == null) {
+private fun printFound(data: List<String>, indices: List<Int>) {
+    if (indices.isEmpty()) {
         println("No matching people found.")
     } else {
-        index[search]?.forEach {
+        println("${indices.size} persons found:")
+        indices.forEach {
             println(data[it])
         }
     }
+}
+
+private fun findPerson(data: List<String>, index: Map<String, MutableList<Int>>) {
+    println()
+    println("Select a matching strategy: ALL, ANY, NONE")
+    val strategy = readLine()!!
+    println()
+    println("Enter a name or email to search all suitable people.")
+    val search = readLine()!!
+    println()
+    val terms = search.split(" ")
+    val list = MutableList(data.size) { it }
+    when (strategy) {
+        "ALL" -> {
+            terms.forEach {
+                index[it.toLowerCase()]?.let { it1 -> list.retainAll(it1) }
+            }
+        }
+        "ANY" -> {
+            list.clear()
+            terms.forEach {
+                index[it.toLowerCase()]?.let { it1 -> list.addAll(it1) }
+            }
+            println(list)
+        }
+        "NONE" -> {
+            terms.forEach {
+                index[it.toLowerCase()]?.let { it1 -> list.removeAll(it1) }
+            }
+        }
+    }
+    printFound(data, list)
 }
 
 private fun createInvertedIndex(data: List<String>): Map<String, MutableList<Int>> {
@@ -47,9 +78,9 @@ private fun createInvertedIndex(data: List<String>): Map<String, MutableList<Int
         index, line -> run {
             val tokens = line.split(" ")
             tokens.forEach {
-                val list = invertedIndex.getOrDefault(it, mutableListOf())
+                val list = invertedIndex.getOrDefault(it.toLowerCase(), mutableListOf())
                 list.add(index)
-                invertedIndex[it] = list
+                invertedIndex[it.toLowerCase()] = list
             }
         }
     }
